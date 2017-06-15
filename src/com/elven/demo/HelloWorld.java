370363fbb3f0f52fd9df4b00355668d01a3b46c1
Elven <799835984@qq.com>
2017-06-15 11:30:55 +0800

    test my idea editor

---
+++ src/com/elven/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.elven.demo;
+
+/**
+ * Created by Elven on 2017/6/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
